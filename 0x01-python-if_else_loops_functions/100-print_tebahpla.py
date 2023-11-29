bfaa4de17fc501dc160f6cdf3496674178a1e76f
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-29 18:53:58 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 output = ""
-upper = True
+upper = True 
 
 for i in range(90, 64, -1):
     if upper:
-        output += "{}".format(chr(i)) 
+        output += "{}".format(chr(i))
     else:
-        output += "{}".format(chr(i).lower())
+        output += "{}".format(chr(i).lower()) 
     upper = not upper
 
-print("{}"[::-1].format(output), end='')
+print("{0}"[::-1].format(output), end='')
