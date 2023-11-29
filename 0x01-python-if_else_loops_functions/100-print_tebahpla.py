d1d58e9ec6b12350333a195c523c7216604d57ec
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-29 18:48:39 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 output = ""
 for i in range(90, 64, -1):
-    output += "{}{}".format(chr(i + 32), chr(i))
+    if i % 2 == 0:
+        output += "{}{}".format(chr(i + 32), chr(i))
 
-print(output, end='')
+print(output[::-1], end='')
