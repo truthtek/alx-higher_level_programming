354c2f7dd614eaaa205344ba0ee2bfee4685682e
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-30 13:03:05 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, subtract, multiply, divide
+def perform_calculations(a, b):
+    """Perform calculations using the provided values."""
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    prod_result = mul(a, b)
+    quot_result = div(a, b)
 
-add_result = add(a, b)
-subtract_result = subtract(a, b)
-multiply_result = multiply(a, b)
-divide_result = divide(a, b)
+    return sum_result, diff_result, prod_result, quot_result
 
-print("{} + {} = {}".format(a, b, add_result))
-print("{} - {} = {}".format(a, b, subtract_result))
-print("{} * {} = {}".format(a, b, multiply_result))
-print("{} / {} = {}".format(a, b, divide_result))
+def print_results(a, b, sum_result, diff_result, prod_result, quot_result):
+    """Print the results of the calculations."""
+    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} - {} = {}".format(a, b, diff_result))
+    print("{} * {} = {}".format(a, b, prod_result))
+    print("{} / {} = {}".format(a, b, quot_result))
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    sum_result, diff_result, prod_result, quot_result = perform_calculations(a, b)
+    print_results(a, b, sum_result, diff_result, prod_result, quot_result)
