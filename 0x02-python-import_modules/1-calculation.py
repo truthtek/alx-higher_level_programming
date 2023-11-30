e2caef45c16ea212d4dd17eba60bfac5eab59881
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-30 13:03:58 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
 
-def perform_calculations(a, b):
-    """Perform calculations using the provided values."""
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
-    sum_result = add(a, b)
-    diff_result = sub(a, b)
-    prod_result = mul(a, b)
-    quot_result = div(a, b)
-
-    return sum_result, diff_result, prod_result, quot_result
-
-def print_results(a, b, sum_result, diff_result, prod_result, quot_result):
-    """Print the results of the calculations."""
-    print("{} + {} = {}".format(a, b, sum_result))
-    print("{} - {} = {}".format(a, b, diff_result))
-    print("{} * {} = {}".format(a, b, prod_result))
-    print("{} / {} = {}".format(a, b, quot_result))
-
-if __name__ == "__main__":
     a = 10
     b = 5
 
-    sum_result, diff_result, prod_result, quot_result = perform_calculations(a, b)
-    print_results(a, b, sum_result, diff_result, prod_result, quot_result)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
