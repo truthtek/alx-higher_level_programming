eb2b02e483592ddea72a11b6c0134abbac5a80a5
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-30 13:07:27 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Print the number and list of command line arguments."""
+
 import sys
 
+def main():
+    """Main function"""
+    args = sys.argv[1:]
+    num_args = len(args)
+    
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+    
+    if num_args > 0:
+        for idx, arg in enumerate(args, start=1):
+            print(f"{idx}: {arg}")
+    else:
+        print(".")
+
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    print("Number of argument{}:{}".format("s" if argc != 1 else "", argc))
-    print(".") if argc == 0 else print("\n".join(["{}: {}".format(i, sys.argv[i])
-                                                  for i in range(1, len(sys.argv))]))
+    main()
