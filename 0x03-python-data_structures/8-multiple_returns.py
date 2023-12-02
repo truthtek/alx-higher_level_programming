9f7305f2f7c388199155633631520398f84f2ec6
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-03 00:54:08 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (None, None)
-    else:
-        return (len(sentence), sentence[0])
+    length = len(sentence)
+    if length == 0:
+        char = None  
+    else: 
+        char = sentence[0]
+    return (length, char)
