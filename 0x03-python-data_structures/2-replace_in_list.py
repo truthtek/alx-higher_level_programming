8755fb089cdde628bfb1c3b514aeb71a97d82a14
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-03 00:37:36 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3]
-new_list = replace_in_list(my_list, 0, 5)
-print(new_list) # [5, 2, 3]  
-
-no_change = replace_in_list(my_list, -1, 5) 
-print(no_change) # [1, 2, 3] - no change
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
