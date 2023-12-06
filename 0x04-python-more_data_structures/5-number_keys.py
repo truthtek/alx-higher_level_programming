d3d9b62fa09134c1a1ed1197d53d156c481d6bfd
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-06 21:19:35 +0100

    Create 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    # Get the keys of the dictionary
+    keys = a_dictionary.keys()
+
+    # Count the number of keys
+    count = len(keys)
+
+    return count
