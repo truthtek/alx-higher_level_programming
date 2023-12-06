5fc7b4beea0fe3db2dea384bacd5b1f5c812135e
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-06 21:20:43 +0100

    Create 6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # Get the sorted keys
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Iterate over the sorted keys and print the key-value pairs
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print(f"{key}: {value}")
