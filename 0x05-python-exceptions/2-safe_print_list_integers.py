9890cf4d69f67f235a223b58baea01270a86b815
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-18 09:07:16 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if isinstance(item, int):
+                print("{:d}".format(item), end=" ")
+                count += 1
+                if count == x:
+                    break
+    except:
+        pass
+
+    print()
+    return count
