e0215eabcefd2bd7286dcdf6b3512aa742ba57f8
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-19 21:59:17 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=" ")
-                count += 1
-    except (IndexError, TypeError):
-        pass
-    finally:
-        print()
-        return count
+"""class Square that defines a square"""
+
+
+class Square():
+    """square class with it's size and proper validation"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if not self.__size:
+            print("")
+        for i in range(self.__size):
+            print("#" * self.__size)
