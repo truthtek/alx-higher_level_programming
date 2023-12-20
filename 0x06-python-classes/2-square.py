0d7f0cc538ac1dfa1eb48f2e7ca4744587539bb6
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-20 21:09:50 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+""" Working on oop with python """
+
 class Square:
+    """Class representing a square"""
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """ Initialize the class
+
+        Args:
+           size (int, optional): size of the square (default is 0)
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        
+        self.__size = size
