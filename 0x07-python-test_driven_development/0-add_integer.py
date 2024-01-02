d7f6bf14fc5a2ca0456dbc4c13b5a0fec2df8c90
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-01-02 08:34:15 +0100

    Create 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing a dummy adder function for testing"""
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
