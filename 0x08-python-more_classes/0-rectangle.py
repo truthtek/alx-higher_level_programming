f06e11a5bddee88927aa472828c35b40d759f700
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-01-04 19:20:45 +0100

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    """
-    Empty class representing a rectangle.
-    No modules are imported as per requirements.
-    """
+    """Represent a rectangle."""
     pass
