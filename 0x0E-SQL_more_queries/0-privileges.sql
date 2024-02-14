eea34ff3ceecad3912b17db00b22e8021d908fca
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-02-14 21:30:24 +0100

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,14 +1,4 @@
--- Create user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_password';
+-- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Create user_0d_2 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_password';
-
--- Grant necessary privileges to user_0d_2
-GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
