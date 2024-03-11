b14811eb97a531871960adf2d945027fb4b153b1
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-11 19:43:56 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
 const arg = process.argv[2];
-
-if (!arg) {
-    console.log("No argument");
+if (arg === undefined) {
+  console.log('No argument');
 } else {
-    console.log(arg);
+  console.log(arg);
 }
