ebb5605c2431722ff560f171999d11341582a4a3
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-12 19:41:35 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0 && typeof w === 'number' && typeof h === 'number') {
-      this.width = w;
-      this.height = h;
-    } else {
-      this = {};
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
-
-module.exports = Rectangle;
+};
