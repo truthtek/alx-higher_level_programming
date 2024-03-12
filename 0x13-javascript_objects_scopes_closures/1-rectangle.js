94cf8a57cb565cef985015866ba1b8e26838780c
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-12 19:40:48 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
+module.exports = class Rectangle {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
-
-module.exports = Rectangle;
+};
