0d82587b0bfa0d68834ff5c882db9dc5f5bc9568
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-18 04:55:52 +0100

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script to list states matching the given name from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database_name)
+
+    # Create a cursor
+    cursor = db.cursor()
+
+    # Execute the query
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+
+    # Fetch and print the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Clean up
+    cursor.close()
+    db.close()
