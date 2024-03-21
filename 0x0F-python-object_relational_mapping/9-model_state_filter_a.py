2256d92198a93e0ef5827b08b64a64de8ef171cb
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-21 19:35:50 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,14 +14,12 @@ password = sys.argv[2]
 database_name = sys.argv[3]
 
 # Create an engine and a session
-engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/
-{database_name}', pool_pre_ping=True)
+engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}', pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # Query State objects that contain the letter 'a' and print them
-states = session.query(State).filter
-(State.name.like('%a%')).order_by(State.id).all()
+states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 for state in states:
 print(f"{state.id}: {state.name}")
 
