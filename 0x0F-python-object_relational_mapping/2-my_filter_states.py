e656e4e0a378ea18d2aef32194994c1d7e61aac6
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-22 21:11:50 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Script to list states matching the given name
-from the database hbtn_0e_0_usa"""
-import MySQLdb
-import sys
+"""Script to list states matching the given name from the database hbtn_0e_0_usa"""
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     # Get arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
 
     # Connect to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
@@ -20,9 +19,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute the query
-    query = "SELECT * FROM states WHERE name = 
-    '{}'ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     # Fetch and print the results
     results = cursor.fetchall()
