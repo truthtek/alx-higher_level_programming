f00a4ab1298783845dce8556e5f06c248c343f26
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-22 21:13:26 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python3
-"""Script to list states matching the given name from the database hbtn_0e_0_usa"""
+
+"""Script to list states matching the given name
+from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
-    # Get arguments
-    username = argv[1]
-    password = argv[2]
-    database_name = argv[3]
-    state_name = argv[4]
+import sys
 
-    # Connect to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
+if name == "__main__":
 
-    # Create a cursor
-    cursor = db.cursor()
+# Get arguments
 
-    # Execute the query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
+username = sys.argv[1]
 
-    # Fetch and print the results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+password = sys.argv[2]
 
-    # Clean up
-    cursor.close()
-    db.close()
+database_name = sys.argv[3]
+
+state_name = sys.argv[4]
+
+# Connect to the database
+
+db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                     passwd=password, db=database_name)
+
+# Create a cursor
+
+cursor = db.cursor()
+
+# Execute the query
+
+query = "SELECT * FROM states WHERE name ='{}'ORDER BY id ASC".format(state_name)
+
+cursor.execute(query)
+
+# Fetch and print the results
+
+results = cursor.fetchall()
+
+for row in results:
+
+print(row)
+
+# Clean up
+
+cursor.close()
+
+db.close()
