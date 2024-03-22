4c217399db6d8b6decf1b5ea88a6a5d26fb2ca11
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-22 21:02:39 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script to list all states with a name starting with 'N' from the database hbtn_0e_0_usa"""
+"""Script to list all states with a name starting with 'N'
+from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
