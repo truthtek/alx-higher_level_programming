338ce5a37a80aac0715b9ee88b765acad8df2b59
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-22 05:35:10 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,26 @@
-
 #!/usr/bin/python3
-# Lists all State objects from the database hbtn_0e_6_usa.
-# Usage: ./7-model_state_fetch_all.py <mysql username> /
-#                                     <mysql password> /
-#                                     <database name>
-import sys
-from sqlalchemy import create_engine
+"""
+Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/{}"""
+                           .format(username, password, database_name),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+    result = session.query(State).order_by(State.id.asc()).all()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
