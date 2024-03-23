a9942efe8c363b67d9b743b1b5cd289a5dbba129
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-23 05:41:41 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""Lists all City objects from the database hbtn_0e_14_usa,
-ordered by state and city name."""
-
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    query = session.query(State.name, City.id, 
-                          City.name).join(City).order_by(State.name, 
-                                                         City.name)
-    for state_name, city_id, city_name in query:
-        print("{}: ({}) {}".format(state_name, 
-                                   city_id, city_name))
-
-    session.close()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
