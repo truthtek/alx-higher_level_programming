6c9c5a35fe3334e3a9f0367467dce5c8780fa977
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:07:05 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script displays all HTTP methods accepted by the server at the given URL
-
-methods=$(curl -s -X OPTIONS "$1" -I | grep "Allow:" | cut -d " " -f2-)
-echo "$methods"
+# takes in a URL and displays all HTTP methods the server will accept
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
