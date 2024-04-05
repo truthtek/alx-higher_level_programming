66b6b5d74159f5640ae504404bdee7e579c77418
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:06:42 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a DELETE request to a URL and displays the body of the response
-
-response=$(curl -s -X DELETE "$1")
-echo "$response"
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE $1 -L
