1f4fd8cdbe2e7e0a750aece06d4bf6b27eeb90af
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:00:25 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL and displays the body of the response if the status code is 200
-
-response=$(curl -s -I "$1" | grep "HTTP/")
-status_code=$(echo "$response" | awk '{print $2}')
-
-if [ "$status_code" -eq 200 ]; then
-    body=$(curl -s "$1")
-    echo "$body"
-fi
+# Sends a GET request to a URL without redirection
+curl -sL -w "%{http_code}" -o /dev/null "$1"
