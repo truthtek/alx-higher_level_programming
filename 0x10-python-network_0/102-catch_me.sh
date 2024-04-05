7e1ed991370f8049d6bb1ef22442746e50253882
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:06:14 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# This script sends a request to 0.0.0.0:5000/catch_me and displays the response body
-
-response=$(curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me)
-
-printf '%s\n' "$response"
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
