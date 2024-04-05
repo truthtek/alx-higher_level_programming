6cdf00fed03490fdfb934e5e40312dffa2c7d775
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:05:49 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request with the contents of a file as the request body
-
-url="$1"
-file="$2"
-
-if [ -n "$file" ] && [ -f "$file" ]; then
-    data=$(cat "$file")
-    response=$(curl -s -X POST -H "Content-Type: application/json" -d "$data" "$url")
-    echo "$response"
-else
-    echo "Error: File not found or invalid filename."
-fi
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
