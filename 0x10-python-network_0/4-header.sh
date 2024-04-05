fdcf2ac6ed861dc6b23708b744e058d039049569
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-05 05:07:37 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a GET request with a custom header and displays the body of the response
-
-response=$(curl -s -X GET -H "X-School-User-Id: 98" "$1")
-echo "$response"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
